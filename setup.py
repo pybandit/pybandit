c36d360ad8a68fc474653991e6c0df86fe34bacf
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-09 17:08:48 +0530

    ENH: add functionality for editable install (#19)

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+# Function to read the list of requirements from requirements.txt
+def read_requirements():
+    with open("requirements.txt") as req:
+        return req.read().splitlines()
+
+
+setup(
+    name="pybandit",
+    version="0.0.1",
+    description="Implementation of popular Multiarmed Bandit Algorithms",
+    authors=["Tuhin Sharma <tuhinsharma121@gmail.com>"],
+    packages=find_packages(),
+    install_requires=read_requirements(),
+)
