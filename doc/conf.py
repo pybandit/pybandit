05a50c5ae4690b3839958f901eaf90b1133b161b
tuhinsharma121 <tuhinsharma121@gmail.com>
2024-05-11 22:40:21 +0530

    ENH: add logo and favicon

--- doc/conf.py
+++ doc/conf.py
@@ -52,39 +52,39 @@ html_theme = "pydata_sphinx_theme"
 html_logo = "_static/logo.png"
 html_favicon = "_static/logo.png"
 #
-# html_theme_options = {
-#     "external_links": [
-#         {
-#             "url": "https://tuhinsharma.com",
-#             "name": "Author",
-#         }
-#     ],
-#     "header_links_before_dropdown": 4,
-#     "icon_links": [
-#         {
-#             "name": "GitHub",
-#             "url": "https://github.com/pybandit/pybandit.git",
-#             "icon": "fa-brands fa-github",
-#         },
-#         {
-#             "name": "PyPI",
-#             "url": "https://pypi.org/project/pybandit",
-#             "icon": "fa-custom fa-pypi",
-#         },
-#         {
-#             "name": "pybandit",
-#             "url": "https://pybandit.org",
-#             "icon": "fa-custom fa-pydata",
-#         },
-#     ],
-#     # alternative way to set twitter and github header icons
-#     # "github_url": "https://github.com/pydata/pydata-sphinx-theme",
-#     # "twitter_url": "https://twitter.com/PyData",
-#     "logo": {
-#         "text": "PyBandit",
-#         "image_dark": "_static/logo.png",
-#     }
-# }
+html_theme_options = {
+    "external_links": [
+        {
+            "url": "https://tuhinsharma.com",
+            "name": "Author",
+        }
+    ],
+    "header_links_before_dropdown": 4,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/pybandit/pybandit.git",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/pybandit",
+            "icon": "fa-custom fa-pypi",
+        },
+        {
+            "name": "pybandit",
+            "url": "https://pybandit.org",
+            "icon": "fa-custom fa-pydata",
+        },
+    ],
+    # alternative way to set twitter and github header icons
+    # "github_url": "https://github.com/pydata/pydata-sphinx-theme",
+    # "twitter_url": "https://twitter.com/PyData",
+    "logo": {
+        "text": "PyBandit",
+        "image_dark": "_static/logo.png",
+    }
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
